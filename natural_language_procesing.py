1e1f3c8eb51c9aa1e0363c62f7b3bcc5f78cc173
Joseorina <orinangare@gmail.com>
2019-11-13 05:12:39 +0300

    NLP

--- natural_language_procesing.py
+++ natural_language_procesing.py
@@ -4,4 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('Restaurant_Reviews.tsv')
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t',quoting=3)
+
+#Cleaning the text
+import re
+review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][0])
+review = review.lower()
+
